99f8a1e1388a9fe3b4c99fe82e246f765cce24d9
Vulpinary <134676992+Vulpinary@users.noreply.github.com>
2025-01-14 13:39:35 +0300

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,20 +1,24 @@
-window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
-
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
-    dom_id: '#swagger-ui',
-    deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
-  });
-
-  //</editor-fold>
+ url: "https://vulpinary.github.io/Vulpy/openapi.json",
+  dom_id: '#swagger-ui',
+  deepLinking: true,
+  presets: [
+    SwaggerUIBundle.presets.apis,
+     SwaggerUIStandalonePreset
+  ],
+     layout: "StandaloneLayout",
+  customOptions: {
+    swaggerUi: {
+       url: "https://vulpinary.github.io/Vulpy/openapi.json",
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+        ],
+        layout: "StandaloneLayout",
+        configUrl: null,
+        showCommonExtensions: false,
+        showExtensions: true
+    }
+  }
 };
